ae1fe95908f086ac9733b6c090aaa0e660e279df
aniketsingh13 <85512784+aniketsingh13@users.noreply.github.com>
2021-09-17 12:03:04 -0700

    beautify files

--- app.js
+++ app.js
@@ -5,36 +5,36 @@ var checkButton = document.querySelector("#chk-button");
 var outputD = document.querySelector("#output");
 
 
-function calculatingStock(initalprice,quantity,current){
-    if(current < initalprice){
-     var loss = (initalprice - current) * quantity;
-     var lossPercentage = (loss/initalprice) * 100;
-     showoutPut(`😔 The loss is ${loss} and the losspercentag is ${lossPercentage}%`)
-    }else if(current > initalprice){
+function calculatingStock(initalprice, quantity, current) {
+    if (current < initalprice) {
+        var loss = (initalprice - current) * quantity;
+        var lossPercentage = (loss / initalprice) * 100;
+        showoutPut(`😔 The loss is ${loss} and the losspercentag is ${lossPercentage}%`)
+    } else if (current > initalprice) {
         var profit = (current - initalprice) * quantity;
-        var profitPercentage = (profit/initalprice) * 100;
+        var profitPercentage = (profit / initalprice) * 100;
         showoutPut(`😊 your profit is ${profit} and profitpercentage is ${profitPercentage}`)
-    } else{
-        showoutPut("No pain no gain,no gain no pain" )
-    }         
+    } else {
+        showoutPut("No pain no gain,no gain no pain")
+    }
 
 
 }
 
-function showoutPut(message){
+function showoutPut(message) {
     outputD.innerText = message;
 }
 
 
-function clickHandler(){
+function clickHandler() {
     var ip = Number(initialPrice.value);
     var qty = Number(quantity.value);
     var cps = Number(currentPrice.value);
-     calculatingStock(ip,qty,cps);
+    calculatingStock(ip, qty, cps);
 
 }
 
 
 
 
-checkButton.addEventListener("click",clickHandler)
+checkButton.addEventListener("click", clickHandler)
